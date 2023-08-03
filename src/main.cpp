7ba2b654c58e53e426b7bb32d4965434cc5a091c
Linas Minkevičius <minkevicius.linas@gmail.com>
2023-08-03 21:17:57 +0300

    added led effects

--- src/main.cpp
+++ src/main.cpp
@@ -77,12 +77,57 @@ void PlayAudio(void *parameter)
   vTaskDelete(nullptr);
 }
 
+void ClearLeds()
+{
+  for (size_t i = 0; i < NUM_LEDS; i++)
+  {
+    leds[i] = CRGB::Black;
+    FastLED.show();
+  }
+}
 void PlayLedBoot()
 {
+  FastLED.setBrightness(255);
+  pinMode(GPIO_NUM_33, INPUT);
+  randomSeed(analogRead(GPIO_NUM_33));
+  uint8_t cyclesToTrigger = random(5, 10);
+  uint8_t cycleDelay = random(30, 100);
+  for (size_t i = 0; i < cyclesToTrigger; i++)
+  {
+    ClearLeds();
+    for (size_t i = 0; i < NUM_LEDS; i++)
+    {
+      uint8_t brigntessDivisor = random(1, 5);
+      uint8_t colorVarience = random(0, 50);
+      leds[i] = CRGB(35 / brigntessDivisor+colorVarience, 152 / brigntessDivisor+colorVarience, 176 / brigntessDivisor+colorVarience)*random(0,2);
+      FastLED.show();
+    }
+    vTaskDelay(cycleDelay / portTICK_PERIOD_MS);
+  }
+  ClearLeds();
 }
 
 void PlayLedShoot()
 {
+  uint16_t shootTime = 6000;
+  for (size_t j = 0; j < 255; j++)
+  {
+    for (size_t i = 0; i < NUM_LEDS; i++)
+    {
+      leds[i] = CRGB::LimeGreen;
+    }
+    FastLED.setBrightness(j);
+    FastLED.show();
+    vTaskDelay((shootTime / 255) / portTICK_PERIOD_MS);
+  }
+
+  uint16_t powerDownTime = 200;
+  for (size_t j = 255; j>0; j--)
+  {
+    FastLED.setBrightness(j);
+    FastLED.show();
+    vTaskDelay((powerDownTime / 255) / portTICK_PERIOD_MS);
+  }
 }
 
 void PlayLeds(void *parameter)
@@ -94,19 +139,6 @@ void PlayLeds(void *parameter)
   {
     PlayLedBoot();
     PlayLedShoot();
-    // play it, this will cause it to repeat and repeat...
-    // Turn the LED on, then pause
-    for (size_t i = 0; i < NUM_LEDS; i++)
-    {
-      leds[i] = CRGB::Red;
-      FastLED.show();
-      vTaskDelay((100 / portTICK_PERIOD_MS) / (i * i + 1));
-      // Now turn the LED off, then pause
-      leds[i] = CRGB::Black;
-      FastLED.show();
-      vTaskDelay((100 / portTICK_PERIOD_MS) / (i * i + 1));
-    }
-    // ledTaskHandle == nullptr;
   }
   vTaskDelay(10);
   taskCounter--;
